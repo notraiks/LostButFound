6d8d810516eb473045d3a98eeec9a0ae9a62be1a
Raiken Lee Ladrera <23101967@usc.edu.ph>
2024-12-14 12:21:06 +0800

    Delete src directory

--- src/php/get_item.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('db_connect.php');
-$id = $_GET['id'];
-$query = mysqli_query($conn, "SELECT * FROM item WHERE Item_ID = '$id'");
-$item = mysqli_fetch_assoc($query);
-
-echo json_encode($item);
-?>
